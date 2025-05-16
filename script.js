2f52bcf0f7df5eb3256fc0a8a33678ec4b048341
nathanfnco <nathan.fnco@outlook.fr>
2025-05-16 09:54:22 +0200

    Maj_communes_selon_le_code_postal

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ const postalCodeInput = document.getElementById("postal-code");
 const citySelect = document.getElementById("city");
 const form = document.getElementById("weather-form");
 const resultSection = document.getElementById("weather-result");
+
+
+// Maj communes selon le code postal
+postalCodeInput.addEventListener("input", async () => {
+  const code = postalCodeInput.value;
+  if (/^\d{5}$/.test(code)) {
+    const res = await fetch(`${geoApiUrl}${code}&fields=nom,code,centre`);
+    const cities = await res.json();
+
+    citySelect.innerHTML = "";
+    cities.forEach(city => {
+      const option = document.createElement("option");
+      option.value = JSON.stringify({ code: city.code, lat: city.centre.coordinates[1], lon: city.centre.coordinates[0] });
+      option.textContent = city.nom;
+      citySelect.appendChild(option);
+    });
+  }
+});
